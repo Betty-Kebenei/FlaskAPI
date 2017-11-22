3a72182b9b7700c24a6ed0ef36d1d891d9546139
Betty Kebenei <kebenei@Bettys-MacBook-Pro.local>
2017-11-22 09:07:43 +0300

    Update status codes returned

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,46 +15,34 @@ def register():
         username = request.data['username']
         email = request.data['email']
         password = request.data['password']
-        user = User.query.filter_by(email=email).first()
-        if not user:
-            user = User(
-                firstname=firstname,
-                lastname=lastname,
-                username=username,
-                email=email,
-                password=password
-            )
-            user.save()
-            response = jsonify(
-                {'message':'User with email {} successfully registered'.format(user.email)}
-                )
-            response.status_code = 201
+        if not username and email and password:
+            response = jsonify({'message':'You did not provide one of the following: email, username or password.'})
+            response.status_code = 400
             return response
         else:
-            response = jsonify({'message':'User with email:{} already exist.'.format(user.email)})
-            response.status_code = 409
-            return response
+            user = User.query.filter_by(email=email).first()
+            if not user:
+                user = User(
+                    firstname=firstname,
+                    lastname=lastname,
+                    username=username,
+                    email=email,
+                    password=password
+                )
+                user.save()
+                response = jsonify(
+                    {'message':'User with email {} successfully registered'.format(user.email)}
+                    )
+                response.status_code = 201
+                return response
+            else:
+                response = jsonify({'message':'User with email:{} already exist.'.format(user.email)})
+                response.status_code = 409
+                return response
     else:
         results = []
-        q = request.args.get('q')
-        if q:
-            users = User.query.filter_by(
-                user_id=user_id).filter(User.username.like('%{0}%'.format(q)))
-        else:
-            users = User.query.filter_by(user_id=user_id)
-
+        users = User.get_all()
         if users:
-            pagination = users.paginate(page, per_page=limit, error_out=False)
-            users = pagination.items
-            if pagination.has_prev:
-                prev = url_for('auth.register', user_id=user_id, page=page-1, limit= limit, _external=True)
-            else:
-                prev = None
-            if pagination.has_next:
-                next = url_for('auth.register', user_id=userid, page=page+1, limit=limit, _external=True)
-            else:
-                next = None
-
             for user in users:
                 obj = {
                     'user_id' : user.user_id,
@@ -64,12 +52,7 @@ def register():
                     'email' : user.email
                     }
                 results.append(obj)
-            response = jsonify({
-                    'users': results,
-                    'prev': prev,
-                    'next': next,
-                    'count': pagination.total
-                    })
+            response = jsonify(results)
             response.status_code = 200
             return response
         else:
@@ -98,28 +81,36 @@ def login():
 
     if request.method == "POST":
         user = User.query.filter_by(email=request.data['email']).first()
-        password = user.verify_password(request.data['password'])
-        if user:
-            if password:
-                access_token = user.encode_auth_token(user.user_id)
-                if access_token:
+        password = request.data['password']
+        if not user and password:
+            response = jsonify({
+                    'message':
+                    'email or password missing!'
+                    })
+            response.status_code = 400
+            return response
+        else:
+            if user:
+                if user.verify_password(password):
+                    access_token = user.encode_auth_token(user.user_id)
+                    if access_token:
+                        response = jsonify({
+                            'message':'Hey {} you are successfully logged in.'.format(user.firstname),
+                            'access_token': access_token.decode('utf-8')
+                            })
+                        response.status_code = 200
+                        return response
+                else:
                     response = jsonify({
-                        'message':'Hey {} you are successfully logged in.'.format(user.firstname),
-                        'access_token': access_token.decode()
-                        })
-                    response.status_code = 200
+                        'message':
+                        'Hey {} you entered incorrect password!'
+                        .format(user.username)})
+                    response.status_code = 404
                     return response
             else:
                 response = jsonify({
                     'message':
-                    'Hey {} you entered incorrect password!'
-                    .format(user.username)})
+                    'Hey no user with such email!'
+                    })
                 response.status_code = 404
                 return response
-        else:
-            response = jsonify({
-                'message':
-                'Hey no user with such email!'
-                })
-            response.status_code = 404
-            return response
