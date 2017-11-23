8a987cd926d018d306d3333c3094c85370b95468
Betty Kebenei <kebenei@Bettys-MacBook-Pro.local>
2017-11-23 15:55:22 +0300

    Add validations

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from . import auth
 from . import auth as auth_blueprint
 from flask import request, jsonify
 from app.models import User
+import re
 
 @auth.route('/auth/register', methods=['POST', 'GET'])
 def register():
@@ -15,11 +16,25 @@ def register():
         username = request.data['username']
         email = request.data['email']
         password = request.data['password']
-        if not username and email and password:
-            response = jsonify({'message':'You did not provide one of the following: email, username or password.'})
-            response.status_code = 400
-            return response
-        else:
+        if username and email and password: 
+            # if not re.match(r'', username):
+            #     response = jsonify(
+            #         {'message':'Username should contain letters, digits and spaces only'}
+            #         )
+            #     response.status_code = 400
+            #     return response
+            #  if not re.match(r'', username):
+            #         response = jsonify(
+            #         {'message':'email should contain letters, digits and spaces only'}
+            #         )
+            #     response.status_code = 400
+            #     return response
+            #  if not re.match(r'', username):
+            #         response = jsonify(
+            #         {'message':'Username should contain letters, digits and spaces only'}
+            #         )
+            #     response.status_code = 400
+            #     return response
             user = User.query.filter_by(email=email).first()
             if not user:
                 user = User(
@@ -38,6 +53,19 @@ def register():
             else:
                 response = jsonify({'message':'User with email:{} already exist.'.format(user.email)})
                 response.status_code = 409
+                return response
+        else:
+            if not username:
+                response = jsonify({'message':'Please provide username!'})
+                response.status_code = 400
+                return response
+            elif not email:
+                response = jsonify({'message':'Please provide email!'})
+                response.status_code = 400
+                return response
+            else:
+                response = jsonify({'message':'Please provide password!'})
+                response.status_code = 400
                 return response
     else:
         results = []
@@ -96,7 +124,7 @@ def login():
                     if access_token:
                         response = jsonify({
                             'message':'Hey {} you are successfully logged in.'.format(user.firstname),
-                            'access_token': access_token
+                            'access_token': access_token.decode()
                             })
                         response.status_code = 200
                         return response
