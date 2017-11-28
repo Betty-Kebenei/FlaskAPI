998c526d127117f8032f9886bfc5c1106d64f038
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-11-28 22:14:17 +0300

    Change FLASK_CONFIG variable to APP_CONFIG

--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@ import os
 from flask_script import Manager # class for handling a set of commands
 from flask_migrate import Migrate, MigrateCommand
 from app import DB, create_app
-from app import models
 
-app = create_app(config_name=os.getenv('FLASK_CONFIG'))
+
+app = create_app(config_name=os.getenv('APP_CONFIG'))
 migrate = Migrate(app, DB)
 manager = Manager(app)
 
